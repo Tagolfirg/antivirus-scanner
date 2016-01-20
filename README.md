82b8869988b12357c06a35e6541224c12ab52e41
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-01-20 16:44:57 +0000

    initial commit

--- README.md
+++ README.md
@@ -3,7 +3,20 @@
 
 [![Build Status](https://travis-ci.org/hmrc/antivirus-scanner.svg?branch=master)](https://travis-ci.org/hmrc/antivirus-scanner) [ ![Download](https://api.bintray.com/packages/hmrc/releases/antivirus-scanner/images/download.svg) ](https://bintray.com/hmrc/releases/antivirus-scanner/_latestVersion)
 
-This is a placeholder README.md for a new repository
+
+
+
+## Installation
+
+This requires [ClamAV](http://www.clamav.net/) to be installed
+
+For Macs
+
+```brew install clamav```
+
+
+
+
 
 ### License
 
---
+++ project/MicroServiceBuild.scala
@@ -0,0 +1,46 @@
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object MicroServiceBuild extends Build with MicroService {
+
+  override val appName = "avscanner"
+
+  override lazy val plugins: Seq[Plugins] = Seq(
+    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+  )
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "4.2.1",
+    "uk.gov.hmrc" %% "play-health" % "1.1.0",
+    "uk.gov.hmrc" %% "play-config" % "2.0.1",
+    "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
+    "uk.gov.hmrc" %% "play-authorisation" % "3.1.0"
+  )
+
+  abstract class TestDependencies(scope: String) {
+    lazy val test : Seq[ModuleID] = Seq(
+      "uk.gov.hmrc" %% "hmrctest" % "1.4.0" % scope,
+      "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+      "org.pegdown" % "pegdown" % "1.6.0" % scope,
+      "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    )
+  }
+
+  object Test extends TestDependencies("test")
+  object IntegrationTest extends TestDependencies("it")
+
+  def apply() = compile ++ Test.test ++ IntegrationTest.test
+
+}
+
