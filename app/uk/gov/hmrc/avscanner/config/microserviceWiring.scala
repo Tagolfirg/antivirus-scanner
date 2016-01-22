5e3bdb135211f528e187072b81b69c734f64e8eb
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-01-22 14:15:01 +0000

    update audit config key

--- app/uk/gov/hmrc/avscanner/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/avscanner/config/microserviceWiring.scala
@@ -71,7 +71,7 @@ object ClamAvConfig {
 
 //Connectors
 object MicroserviceAuditConnector extends AuditConnector with RunMode {
-  override lazy val auditingConfig = LoadAuditingConfig(s"$env.auditing")
+  override lazy val auditingConfig = LoadAuditingConfig(s"auditing")
 }
 
 object MicroserviceAuthConnector extends AuthConnector with ServicesConfig {
